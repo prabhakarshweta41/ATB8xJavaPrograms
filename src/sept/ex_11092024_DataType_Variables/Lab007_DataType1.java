2af596302d19ebd1f0d787b000ce7317d6cefb86
Shweta <prabhakarshweta41@gmail.com>
2024-09-12 20:40:56 +0530

    Updated Primitive and Non-Primitive Data Types Updated char Data Types

---
+++ src/sept/ex_11092024_DataType_Variables/Lab007_DataType1.java
@@ -0,0 +1,21 @@
+package sept.ex_11092024_DataType_Variables;
+
+public class Lab007_DataType1 {
+    public static void main(String[] args) {
+      //Store the distance between earth and moon
+        long distance_between_earth_moon = 384400000l;
+        double distance_between_earth_moon2 = 384400000.456;
+
+        //max age
+        byte age = 122;
+        int age1 = 100;   //wasting memory 214567893456
+        //Mostly we will not take care of this data type memory wastage bcoz now-a-days
+
+        //we have more memory in our systems/laptops
+
+        //In real life in automation we usually use 'int' & 'double'
+
+
+    }
+
+}
---
+++ src/sept/ex_11092024_DataType_Variables/Lab008_DataType2.java
@@ -0,0 +1,18 @@
+package sept.ex_11092024_DataType_Variables;
+
+public class Lab008_DataType2 {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println(c);
+
+        char c1 = '$';
+        System.out.println(c1);
+
+        char c2 = '\n';  //Escape character - Which gives new line
+        System.out.println(c2);
+
+        char c3 = '\u1F6A';  //Unicode characters
+        System.out.println(c3);  // :)
+    }
+
+}
