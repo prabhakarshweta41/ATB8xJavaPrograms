2687d5206c3d31dc4c3b0bde638cfe9b7dd1d32d
Shweta <prabhakarshweta41@gmail.com>
2024-09-12 19:27:32 +0530

    Updated boolean data type class and underscore class

---
+++ src/sept/ex_09092024/Lab005.java
@@ -0,0 +1,11 @@
+package sept.ex_09092024;
+
+public class Lab005 {
+
+    public static void main(String[] args) {
+
+        boolean is_java_easy = true;
+        boolean is_automation_testing_easy = true;
+        boolean do_you_have_audi = false;
+    }
+}
---
+++ src/sept/ex_09092024/Lab_underscore_allowed_in_class_name.java
@@ -0,0 +1,4 @@
+package sept.ex_09092024;
+
+public class Lab_underscore_allowed_in_class_name {
+}
