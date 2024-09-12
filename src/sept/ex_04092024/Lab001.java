68ff00cb8669abf071597952f5f7ad0bc5179dae
Shweta <prabhakarshweta41@gmail.com>
2024-09-12 18:42:00 +0530

    Updated comments with cntrl+/ for single line comment and /* ....*/ for multiline comments

--- src/sept/ex_04092024/Lab001.java
+++ src/sept/ex_04092024/Lab001.java
@@ -1,5 +1,5 @@
 package sept.ex_04092024;
 
 public class Lab001 {
-
+    }
 }
---
+++ src/sept/ex_06092024/Lab004.java
@@ -0,0 +1,22 @@
+package sept.ex_06092024;
+
+//This program is for comments
+public class Lab004 {
+
+    public static void main(String[] args){
+        System.out.println("Hello World !!");
+
+        //        Below line is for printing in console
+        System.out.println("Hello World !!");
+
+        /*Below lines are for multiline comments
+        System.out.println("Hii");
+        System.out.println("Hii");
+        System.out.println("Hii");
+        System.out.println("Hii");
+        System.out.println("Hii");   */
+
+
+    }
+
+}
