298cfc93c71cb34a3b80d5b8da535c9c202819b3
Shweta <prabhakarshweta41@gmail.com>
2024-09-12 20:22:11 +0530

    Updated Primitive and Non-Primitive Data Types

---
+++ src/sept/ex_11092024_DataType_Variables/Lab006_DataType.java
@@ -0,0 +1,28 @@
+package sept.ex_11092024_DataType_Variables;
+
+public class Lab006_DataType {
+    public static void main(String[] args) {
+        //data type variable_name(Identifier) = variable_value(literal)
+
+        //Primitive Data Types :-- (Pre-defined Data Types)
+        byte b = 100;
+        short s = 128;
+        int i = 1000;
+        long phone_number = 9876543210l;
+        long phone_number1 = 9876543210L;
+
+        char grade = 'A';
+
+        float f = 3.14f;
+        float f2 = 3.14F;
+
+        double d = 118.24566956453;
+
+        //Non-Primitive Data Types :-- (User-defined Data Types)
+        String name = "Shweta";
+        //Array, Class, ArrayList, HashMap, Hashset
+
+
+
+    }
+}
